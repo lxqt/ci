61c8fc02159460c0f7cdb6bac80876b8d4a6aa67
Chih-Hsuan Yen <yan12125@gmail.com>
2021-05-06 12:49:57 +0800

    Drop workarounds for https://github.com/actions/virtual-environments/issues/2658

--- prepare-archlinux.sh
+++ prepare-archlinux.sh
@@ -4,11 +4,4 @@ Server = https://repo.archlinuxcn.org/\$arch
 SigLevel = Never
 EOF
 
-# Work-around the issue with glibc 2.33 on old Docker engines
-# Extract files directly as pacman is also affected by the issue
-patched_glibc=glibc-linux4-2.33-4-x86_64.pkg.tar.zst
-curl -LO https://repo.archlinuxcn.org/x86_64/$patched_glibc
-bsdtar -C / -xvf $patched_glibc
-sed -r -i 's/^#?IgnorePkg\s*=\s*(.*)/IgnorePkg = \1 glibc/' /etc/pacman.conf
-
 pacman -Syu --noconfirm
