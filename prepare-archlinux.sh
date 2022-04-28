a792e1bf9ad2935e795ecc4450663c570af6a7d8
Chih-Hsuan Yen <yan12125@gmail.com>
2022-04-28 20:41:22 +0800

    Init archlinux keyring and re-enable key checking for [archlinuxcn]
    This is originally a fix for [1]. I decided to re-enable key checking
    for [archlinuxcn] as the keyring needs to be initialized anyway.
    
    [1] https://github.com/lxqt/lxqt-panel/pull/1781#issuecomment-1111755255

--- prepare-archlinux.sh
+++ prepare-archlinux.sh
@@ -1,7 +1,25 @@
+# The key used by lilac, the build bot for archlinuxcn repo
+# https://github.com/archlinuxcn/archlinuxcn-keyring/blob/20220331/master-keyids#L3
+lilac_key=83f817213361bf5f02e7e124f9f9fa97a403f63e
+
 cat >> /etc/pacman.conf <<EOF
 [archlinuxcn]
 Server = https://repo.archlinuxcn.org/\$arch
-SigLevel = Never
 EOF
 
+# Needed to allow updating archlinux-keyring package (see below).
+# This also enables usage of third-party keys [1].
+pacman-key --init
+
+curl "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x$lilac_key" | pacman-key --add -
+pacman-key --lsign-key $lilac_key
+
+# Update the keyring first to allow installing packages signed by newer keys [1].
+# This may be needed when `pacman -Syu` below or other pacman commands in individual LXQt
+# components uses packages signed by newer keys.
+pacman -Sy --noconfirm archlinux-keyring
+
 pacman -Syu --noconfirm
+
+# [1] https://gitlab.archlinux.org/archlinux/archlinux-docker/-/issues/18
+# [2] https://github.com/lxqt/lxqt-panel/pull/1781#issuecomment-1111755255
